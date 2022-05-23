4082b3723c1663822b5843a9291130b38cb35e78
Gemilang <gilgilang15@gmail.com>
2022-05-23 09:09:37 +0700

    minor fix

--- api/client/payment.go
+++ api/client/payment.go
@@ -27,14 +27,15 @@ func PaymentMethod(c echo.Context) error {
 func OrderPayment(c echo.Context) error {
 	//post
 	type Payload struct {
-		Id_order string `json:"id_order"`
+		Id_method int    `json:"id_method"`
+		Id_order  string `json:"id_order"`
 	}
 	var payload Payload
 	if err := json.NewDecoder(c.Request().Body).Decode(&payload); err != nil {
 		return echo.ErrBadRequest
 	}
 	db := database.GetDBInstance()
-	err := db.Raw("update order_payment set is_paid=true where id_order=?", payload).Scan(&payload).Error
+	err := db.Raw("update order_payment set is_paid=true , id_method = ? where id_order=?", payload.Id_method, payload.Id_order).Scan(&payload).Error
 	if err != nil {
 		return echo.ErrInternalServerError
 	}
