18cff76abfe1655e3677dd464bed7a8df569c2b6
Gemilang <gilgilang15@gmail.com>
2022-05-23 07:13:34 +0700

    + param list freelance

--- api/client/listFreelance.go
+++ api/client/listFreelance.go
@@ -36,6 +36,9 @@ type Response struct {
 
 // parameter longitude, latitude,
 func ListFreelance(c echo.Context) error {
+	long := c.Param("long")
+	lat := c.Param("lat")
+	job_code := c.Param("job_code")
 
 	var payload Payload
 	if err := json.NewDecoder(c.Request().Body).Decode(&payload); err != nil {
@@ -48,8 +51,8 @@ func ListFreelance(c echo.Context) error {
 	err := db.Raw(`SELECT u."name" , fd.rating , fd.profile_pict , fd.job_done , fd.points , fd.is_trainee,fd.is_male , jcc.job_child_name ,(6371 * acos( cos( radians(fd.address_lat) ) * cos( radians( ? ) ) *cos( radians( ? ) - radians(fd.address_long) ) 
 	+ sin( radians(fd.address_lat) ) * sin( radians( ? ) )) ) as distance 
 	from freelance_data fd, job_child_code jcc ,job_code jc  , "user" u 
-	where distance <10 and jcc.job_code  = jc.job_code and fd.job_child_code =jcc.job_child_code and u.id_user = fd.id_user 
-	order by distance asc`, payload.AddressLat, payload.AddressLong, payload.AddressLat).Scan(&result).Error
+	where distance <10 and jcc.job_code  = jc.job_code and fd.job_child_code =jcc.job_child_code and u.id_user = fd.id_user and jc.job_code=?
+	order by distance asc`, lat, long, lat, job_code).Scan(&result).Error
 	if err != nil {
 		return echo.ErrInternalServerError
 	}
