ebdd2b2af0e546a43891b6ea8b4cf731f2172275
Gemilang <gilgilang15@gmail.com>
2022-05-23 16:27:32 +0700

    adjust json response

--- api/client/dataPersonal.go
+++ api/client/dataPersonal.go
@@ -11,27 +11,24 @@ import (
 
 func DataPersonal(c echo.Context) error {
 	type Result struct {
-		IdClient    int `gorm:"primaryKey;autoIncrement;"`
-		IdUser      string
-		Name        string
-		HousePict   string
-		PhoneNumber string
-		Address     string
-		AddressLong float64
-		AddressLat  float64
-		IsMale      bool
-		Dob         time.Time
-		Nik         string
-		ProfilePict string
-		CreatedAt   time.Time
-		UpdatedAt   time.Time
+		Name        string    `json:"name"`
+		Email       string    `json:"email"`
+		HousePict   string    `json:"house_pict"`
+		PhoneNumber string    `json:"no_wa"`
+		Address     string    `json:"address"`
+		AddressLong float64   `json:"address_long"`
+		AddressLat  float64   `json:"address_lat"`
+		IsMale      bool      `json:"is_male"`
+		Dob         time.Time `json:"dob"`
+		Nik         string    `json:"nik"`
+		ProfilePict string    `json:"profile_pict"`
 	}
 	var result Result
 
 	uId, _ := helper.ExtractToken(c)
 
 	db := database.GetDBInstance()
-	err := db.Raw(`select u."name" ,cd.house_pict ,cd.address ,cd.address_long,cd.address_lat ,cd.is_male ,cd.dob ,cd.nik ,cd.profile_pict  
+	err := db.Raw(`select u.email,u.no_wa,u."name" ,cd.house_pict ,cd.address ,cd.address_long,cd.address_lat ,cd.is_male ,cd.dob ,cd.nik ,cd.profile_pict  
 	from "user" u, client_data cd 
 	where u.id_user = cd.id_user and u.id_user=?`, uId).Scan(&result).Error
 	if err != nil {
