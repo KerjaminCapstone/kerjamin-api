41e8ec3a7b8bc6affce5caf76ce28c7597a018cc
Gemilang <gilgilang15@gmail.com>
2022-05-24 12:21:00 +0700

    minor fix list freelance

--- api/client/listFreelance.go
+++ api/client/listFreelance.go
@@ -10,16 +10,13 @@ import (
 
 type Response struct {
 	IdFreelance int       `json:"id_freelance"`
+	Name        string    `json:"name"`
 	IsTrainee   bool      `json:"is_trainee"`
 	Rating      float64   `json:"rating"`
 	JobDone     int       `json:"job_done"`
 	DateJoin    time.Time `json:"date_join"`
-	Address     string    `json:"address"`
-	AddressLong float64   `json:"address_long"`
-	AddressLat  float64   `json:"address_lat"`
 	IsMale      bool      `json:"is_male"`
 	Dob         time.Time `json:"dob"`
-	Nik         string    `json:"nik"`
 	ProfilePict string    `json:"profile_pict"`
 	Distance    float64   `json:"distance"`
 }
@@ -33,11 +30,13 @@ func ListFreelance(c echo.Context) error {
 	var result []Response
 
 	db := database.GetDBInstance()
-	err := db.Raw(`SELECT u."name" , fd.rating , fd.profile_pict , fd.job_done , fd.points , fd.is_trainee,fd.is_male , jcc.job_child_name ,(6371 * acos( cos( radians(fd.address_lat) ) * cos( radians( ? ) ) *cos( radians( ? ) - radians(fd.address_long) ) 
+	err := db.Raw(`SELECT fd.date_join, fd.id_freelance,u."name" , fd.rating , fd.profile_pict , fd.job_done , fd.points , fd.is_trainee,fd.is_male , jcc.job_child_name ,(6371 * acos( cos( radians(fd.address_lat) ) * cos( radians( ? ) ) *cos( radians( ? ) - radians(fd.address_long) ) 
 	+ sin( radians(fd.address_lat) ) * sin( radians( ? ) )) ) as distance 
 	from freelance_data fd, job_child_code jcc ,job_code jc  , "user" u 
-	where distance <10 and jcc.job_code  = jc.job_code and fd.job_child_code =jcc.job_child_code and u.id_user = fd.id_user and jc.job_code=?
-	order by distance asc`, lat, long, lat, job_code).Scan(&result).Error
+	where jcc.job_code  = jc.job_code and fd.job_child_code =jcc.job_child_code and u.id_user = fd.id_user and jc.job_code=? and
+	(6371 * acos( cos( radians(fd.address_lat) ) * cos( radians( ? ) ) *cos( radians( ? ) - radians(fd.address_long) ) 
+	+ sin( radians(fd.address_lat) ) * sin( radians( ? ) )) ) <10
+	order by distance asc`, lat, long, lat, job_code, lat, long, lat).Scan(&result).Error
 	if err != nil {
 		return echo.ErrInternalServerError
 	}
